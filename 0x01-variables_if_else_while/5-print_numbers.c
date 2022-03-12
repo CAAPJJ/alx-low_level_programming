3d38a4def57c51c0e108931d28b434cd4f875d28
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-11 16:18:33 -0800

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+for task 5:
+
+#include <stdio.h>
+
+/**
+   * main - print digits of base ten
+   *
+   * Return: zero
+   */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
