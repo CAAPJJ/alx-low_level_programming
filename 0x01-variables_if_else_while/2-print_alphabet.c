e3d279ef53f3d39fe97e82808820efa1d70c0fe6
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-11 16:10:41 -0800

    IfElse

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
-   * main - Checks last digit
+   * main - print alphabet in lowercase
    *
    *Return: zero
    */
 int main(void)
 {
-int n;
-int l;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-
-if (l > 5)
-{
-    printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{
-    printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else if (l < 6 && l != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
 return (0);
 }
