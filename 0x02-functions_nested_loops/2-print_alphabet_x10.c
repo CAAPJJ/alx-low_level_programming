e7e5860ee56438b22e8715998f1666fe3c6e6e84
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-15 15:34:28 -0800

    alpha_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times in lowercase
+ *
+ **/
+void print_alphabet_x10(void)
+{int x;
+for (x =0;x<=9;x++)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+}
+_putchar('\n');
+}
