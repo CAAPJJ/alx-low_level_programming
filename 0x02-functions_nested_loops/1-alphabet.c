66d0fc1c6bf843053bb838689d16a1e418144e17
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-15 15:05:54 -0800

    erua

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
  * main - check the code for Holberton School students.
  *
- * Return: Always 0.
+ 
  */
 void print_alphabet(void)
 {
@@ -11,5 +10,4 @@ void print_alphabet(void)
 for (i = 'a'; i <= 'z'; i++)
     _putchat(i);
 _putchar('\n');
-return (0);
 }
