d6784eccdb9cf80b88eae7a40fcb42219ca06785
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-15 16:01:38 -0800

    check lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _islower - returns 1 if lowercase alphabet and 0 otherwise
- * @x: takes in a character
- * return: : 0 for uppercase, 1 for lowercase
- **/
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
 int _islower(int x)
 {
 	if (x >= 'a' && x <= 'z')
