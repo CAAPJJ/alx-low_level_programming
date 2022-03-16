bd3a6b18725898efcb7d78304b33b7034b098bee
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-15 16:46:07 -0800

    add two intigers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two numbers
+ * @x: takes in an integer
+ * @y: takes in an integer
+ * Return: x + y result
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
