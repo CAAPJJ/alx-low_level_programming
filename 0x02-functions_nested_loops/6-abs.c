e91558470fae78165e8867b8c86ec77e5fdb18f1
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-15 16:16:10 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - function that prints the absolute value of a number
+ * @x: takes in an integer
+ * Return: 1 if greaterthan 0, 0 if equal 0, positive if negative
+ */
+int _abs(int x)
+{
+if (x < 0)	
+return (x * -1);
+return (x);
+}
