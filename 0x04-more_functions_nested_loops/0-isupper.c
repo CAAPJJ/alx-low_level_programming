d6f319b4eea55db01674642865b89754193866d4
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-17 13:35:00 -0800

    checks if letter is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
-/*
- * _isupper - checks if it is uppercase character.
- * @i: input character.
- * Return: 1 if uppercase character, 0 if its not uppercase.
+
+/**
+ * _isupper - checks if uppercase
+ * @c: characther
+ * Return: returns 1 if uppercase or 0 if not
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
