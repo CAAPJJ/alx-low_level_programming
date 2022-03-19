d7cc66020227ab06169d6be2304aad10f66ba286
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-18 19:04:42 -0800

    prints intiger

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,35 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer
- * @n: input
- * Return: void
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
-	unsigned int i, j, count;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		i = n * -1;
+		m = n * -1;
 	}
 	else
 	{
-		i = n;
+		m = n;
 	}
 
-	j = i;
-	k = 1;
+	d = m;
+	count = 1;
 
-	while (j > 9)
+	while (d > 9)
 	{
-		j /= 10;
-		k *= 10;
+		d /= 10;
+		count *= 10;
 	}
 
-	for (; k >= 1; k /= 10)
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(((i / k) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
