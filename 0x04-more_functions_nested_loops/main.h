84e3ca93da02d1c4265331b1114c1dd41bb53f95
C4PJJ <99596460+C4PJJ@users.noreply.github.com>
2022-03-18 17:43:15 -0800

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef INV_MAIN_H
+#define INV_MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers(void);
+void print_most_numbers(void)u;
 void print_triangle(int size);
 #endif
